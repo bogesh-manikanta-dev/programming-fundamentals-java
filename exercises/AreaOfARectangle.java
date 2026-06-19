deca89fd36e375fb6705a8520ee19112b8c29ba8
bogesh-manikanta-dev <bogeshmanikanta@gmail.com>
2026-06-19 18:31:42 +0530

    ✅ Add solution for: Area of a rectangle.

---
+++ exercises/AreaOfARectangle.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Solution { 
+    public static void main(String[] args) { 
+        Scanner scanner = new Scanner(System.in);
+
+        int length = scanner.nextInt(); 
+        int width = scanner.nextInt();
+
+        // write your code here
+int area = length * width;
+System.out.print(area);
+        scanner.close(); 
+    } 
+}
