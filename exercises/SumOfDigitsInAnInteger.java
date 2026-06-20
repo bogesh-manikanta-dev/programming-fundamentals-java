79a747f507218a643374b9e9b0b9538de9a4d5d5
bogesh-manikanta-dev <bogeshmanikanta@gmail.com>
2026-06-20 21:14:08 +0530

    ✅ Add solution for: Sum of Digits in an Integer.

---
+++ exercises/SumOfDigitsInAnInteger.java
@@ -0,0 +1,29 @@
+import java.util.Scanner; 
+
+public class Solution{ 
+
+    public static void main(String[] args) { 
+
+    Scanner scanner = new Scanner(System.in); 
+    int number = scanner.nextInt();
+        
+        int sum = 0;
+        
+        
+        while (number > 0) {
+            
+            sum += number % 10;
+            
+            number = number / 10;
+        }
+        
+        
+        System.out.println(sum);
+
+    // Write your code here 
+
+    scanner.close();   
+
+    } 
+
+} 
